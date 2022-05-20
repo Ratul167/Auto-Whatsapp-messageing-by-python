da5ab2a87409d3e1b3fa871ec913739b7ce41f47
Ratul Biswas <98582203+Ratul167@users.noreply.github.com>
2022-05-20 12:19:38 +0530

    First Commit
    This can be use when you want to send any message to someone at a specific time. This code will open whats app  and send message in the given time ..........
    
    Enjoy

---
+++ autoWhatsaoppbot.py
@@ -0,0 +1,11 @@
+import  pywhatkit
+
+
+pywhatkit.sendwhatmsg('','',12,20)
+
+
+#first one is number f the person you want to send the message 
+#second one is the message that you want to send 
+#thirs one is the Time (Hours )
+#forth one is Time (Min)
+
